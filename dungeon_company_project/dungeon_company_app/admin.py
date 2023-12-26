4ab6cbcd7601652bd12f0ae5bc298745b62f9afc
NiviroWils <145367856+NiviroWils@users.noreply.github.com>
2023-12-26 23:41:56 +0700

    Delete dungeon_company_project directory

--- dungeon_company_project/dungeon_company_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'game_rules')
-
-admin.site.register(Product, ProductAdmin)
